0ed77cfbca8ae9caf73f4b9c593cc3da82b95879
jagg98 <alejandrogarciagomeztst@gmail.com>
2019-03-07 23:31:02 -0500

    Interface de login

---
+++ ReCogidas/build/generated-sources/ap-source-output/Entitys/Paquetes_.java
@@ -0,0 +1,22 @@
+package Entitys;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-07T23:29:06")
+@StaticMetamodel(Paquetes.class)
+public class Paquetes_ { 
+
+    public static volatile SingularAttribute<Paquetes, String> estadoActual;
+    public static volatile SingularAttribute<Paquetes, String> ciudadOrigen;
+    public static volatile SingularAttribute<Paquetes, Integer> idPersEncargada;
+    public static volatile SingularAttribute<Paquetes, Date> fechaEstEntr;
+    public static volatile SingularAttribute<Paquetes, String> direccion;
+    public static volatile SingularAttribute<Paquetes, Date> fechaRecogida;
+    public static volatile SingularAttribute<Paquetes, Integer> id;
+    public static volatile SingularAttribute<Paquetes, String> ciudadDestino;
+    public static volatile SingularAttribute<Paquetes, String> destinatario;
+
+}
---
+++ ReCogidas/build/generated-sources/ap-source-output/Entitys/Users_.java
@@ -0,0 +1,15 @@
+package Entitys;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-07T23:29:06")
+@StaticMetamodel(Users.class)
+public class Users_ { 
+
+    public static volatile SingularAttribute<Users, String> password;
+    public static volatile SingularAttribute<Users, Integer> id;
+    public static volatile SingularAttribute<Users, String> username;
+
+}
