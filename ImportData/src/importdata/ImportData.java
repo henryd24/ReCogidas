94dd77d917aea85bbd7fa9da5ba854fe8e057179
victorcanno <lpdaska@gmail.com>
2019-03-10 18:03:12 -0500

    Mejora Excel
    Se mejora el test de carga a excel y se añade el archivo correspondiente

--- ImportData/src/importdata/ImportData.java
+++ ImportData/src/importdata/ImportData.java
@@ -22,35 +22,31 @@ public class ImportData {
     public static void main(String[] args) {
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/test","root","");
-            con.setAutoCommit(false);
-            PreparedStatement pstm = null ;
-            FileInputStream input = new FileInputStream("D://file.xls");
-            POIFSFileSystem fs = new POIFSFileSystem( input );
-            HSSFWorkbook wb = new HSSFWorkbook(fs);
-            HSSFSheet sheet = wb.getSheetAt(0);
-            Row row;
-            for(int i=1; i<=sheet.getLastRowNum(); i++){
-                row = sheet.getRow(i);
-                int id = (int) row.getCell(0).getNumericCellValue();
-                String name = row.getCell(1).getStringCellValue();
-                String address = row.getCell(2).getStringCellValue();
-                String sql = "INSERT INTO tablename VALUES('"+id+"','"+name+"','"+address+"')";
-                pstm = (PreparedStatement) con.prepareStatement(sql);
-                pstm.execute();
-                System.out.println("Import rows "+i);
+            FileInputStream input;
+            try (Connection con = (Connection) DriverManager.getConnection("jdbc:derby://localhost:1527/RecogidasDB","app","app")) {
+                con.setAutoCommit(false);
+                PreparedStatement pstm = null ;
+                input = new FileInputStream("D://file.xls");
+                POIFSFileSystem fs = new POIFSFileSystem( input );
+                HSSFWorkbook wb = new HSSFWorkbook(fs);
+                HSSFSheet sheet = wb.getSheetAt(0);
+                Row row;
+                for(int i=1; i<=sheet.getLastRowNum(); i++){
+                    row = sheet.getRow(i);
+                    int id = (int) row.getCell(0).getNumericCellValue();
+                    String name = row.getCell(1).getStringCellValue();
+                    String address = row.getCell(2).getStringCellValue();
+                    String sql = "INSERT INTO PAQUETES VALUES('"+id+"','"+name+"','"+address+"')";
+                    pstm = (PreparedStatement) con.prepareStatement(sql);
+                    pstm.execute();
+                    System.out.println("Import rows "+i);
+                }   con.commit();
+                pstm.close();
             }
-            con.commit();
-            pstm.close();
-            con.close();
             input.close();
             System.out.println("Success import excel to mysql table");
-        }catch(ClassNotFoundException e){
+        }catch(ClassNotFoundException | SQLException | IOException e){
             System.out.println(e);
-        }catch(SQLException ex){
-            System.out.println(ex);
-        }catch(IOException ioe){
-            System.out.println(ioe);
         }
 
     }
