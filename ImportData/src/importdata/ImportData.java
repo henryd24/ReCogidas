575a67a64b1b41127128f105c2632af0ba708e6b
victorcanno <lpdaska@gmail.com>
2019-03-10 16:57:18 -0500

    Prueba Excel
    Se añade teset de importar de excel

---
+++ ImportData/src/importdata/ImportData.java
@@ -0,0 +1,58 @@
+package importdata;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Row;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+
+
+public class ImportData {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/test","root","");
+            con.setAutoCommit(false);
+            PreparedStatement pstm = null ;
+            FileInputStream input = new FileInputStream("D://file.xls");
+            POIFSFileSystem fs = new POIFSFileSystem( input );
+            HSSFWorkbook wb = new HSSFWorkbook(fs);
+            HSSFSheet sheet = wb.getSheetAt(0);
+            Row row;
+            for(int i=1; i<=sheet.getLastRowNum(); i++){
+                row = sheet.getRow(i);
+                int id = (int) row.getCell(0).getNumericCellValue();
+                String name = row.getCell(1).getStringCellValue();
+                String address = row.getCell(2).getStringCellValue();
+                String sql = "INSERT INTO tablename VALUES('"+id+"','"+name+"','"+address+"')";
+                pstm = (PreparedStatement) con.prepareStatement(sql);
+                pstm.execute();
+                System.out.println("Import rows "+i);
+            }
+            con.commit();
+            pstm.close();
+            con.close();
+            input.close();
+            System.out.println("Success import excel to mysql table");
+        }catch(ClassNotFoundException e){
+            System.out.println(e);
+        }catch(SQLException ex){
+            System.out.println(ex);
+        }catch(IOException ioe){
+            System.out.println(ioe);
+        }
+
+    }
+
+}
