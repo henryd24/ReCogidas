9d947ad0657da32d1ba1ba2aa7318683b992c214
henryd24 <henrydanielmm@gmail.com>
2019-03-10 16:49:55 -0500

    Arreglo de errores

--- ReCogidas/build/generated-sources/ap-source-output/Entitys/Users_.java
+++ ReCogidas/build/generated-sources/ap-source-output/Entitys/Users_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-10T15:20:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-07T23:29:06")
 @StaticMetamodel(Users.class)
 public class Users_ { 
 
