a9ddab36a199a01fdab6569ab4a91cfb8f50751e
victorcanno <lpdaska@gmail.com>
2019-03-10 22:49:51 -0500

    CSV
    Se mejora la carga desde csv

--- ImportData/src/importdata/ImportData.java
+++ ImportData/src/importdata/ImportData.java
@@ -1,19 +1,5 @@
 package importdata;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Row;
-
 
 public class ImportData {
 
--- ImportData/src/importdata/Importar.java
+++ ImportData/src/importdata/Importar.java
@@ -5,18 +5,14 @@
  */
 package importdata;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileReader;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+
+import com.opencsv.CSVReader;
+import java.io.IOException;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Row;
 
 /**
  *
@@ -31,41 +27,42 @@ public class Importar {
 
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
-            FileInputStream input;
+            CSVReader reader = new CSVReader(new FileReader("src//resources/PAQUETES.csv"), ',');
             try (Connection con = (Connection) DriverManager.getConnection(dir, "app", "app")) {
                 con.setAutoCommit(false);
                 PreparedStatement pstm = null;
-                input = new FileInputStream("src//resources/PAQUETES.xls");
-                POIFSFileSystem fs = new POIFSFileSystem(input);
-                HSSFWorkbook wb = new HSSFWorkbook(fs);
-                HSSFSheet sheet = wb.getSheetAt(0);
-                Row row;
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-                    row = sheet.getRow(i);
-                    int id = (int) row.getCell(0).getNumericCellValue();
-                    Date datfechare = row.getCell(1).getDateCellValue();
-                    String fechare = sdf.format(datfechare);
-                    Date datfechaes = row.getCell(2).getDateCellValue();
-                    String fechaes = sdf.format(datfechaes);
-                    String ciudador = row.getCell(3).getStringCellValue();
-                    String ciudaddes = row.getCell(4).getStringCellValue();
-                    String direccion = row.getCell(5).getStringCellValue();
-                    String destina = row.getCell(6).getStringCellValue();
-                    String estado = row.getCell(7).getStringCellValue();
-                    int idencar = (int) row.getCell(8).getNumericCellValue();
+                String[] rowData;
+
+                int i = 0;
+                String[] data;
+
+                while ((rowData = reader.readNext()) != null) {
+
+                    data = rowData[0].split(";");
+                    int id = Integer.parseInt(data[0]);
+                    String fechare = data[1];
+                    System.out.println(fechare);
+                    String fechaes = data[2];
+                    String ciudador = data[3];
+                    String ciudaddes = data[4];
+                    String direccion = data[5];
+                    String destina = data[6];
+                    String estado = data[7];
+                    int idencar = Integer.parseInt(data[8]);
                     String sql = "INSERT INTO PAQUETES VALUES(" + id + ",'" + fechare + "','" + fechaes
                             + "','" + ciudador + "','" + ciudaddes + "','" + direccion + "','" + destina + "','" + estado + "'," + idencar + ")";
                     pstm = (PreparedStatement) con.prepareStatement(sql);
                     pstm.execute();
                     System.out.println("Importando fila: " + i);
+                    i++;
                 }
+                System.out.println("exit" + i);
                 con.commit();
                 pstm.close();
+
             }
-            input.close();
-            System.out.println("Importar de Excel a SQL terminado");
-        } catch (ClassNotFoundException | SQLException | IOException e) {
+            System.out.println("Importar de CSV a SQL terminado");
+        } catch (ClassNotFoundException | SQLException | IOException | NumberFormatException e) {
             System.out.println(e);
         }
 
@@ -75,33 +72,35 @@ public class Importar {
 
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
-            FileInputStream input;
+            CSVReader reader = new CSVReader(new FileReader("src//resources/USUARIOS.csv"), ',');
             try (Connection con = (Connection) DriverManager.getConnection(dir, "app", "app")) {
                 con.setAutoCommit(false);
                 PreparedStatement pstm = null;
-                input = new FileInputStream("src//resources/USUARIOS.xls");
-                POIFSFileSystem fs = new POIFSFileSystem(input);
-                HSSFWorkbook wb = new HSSFWorkbook(fs);
-                HSSFSheet sheet = wb.getSheetAt(0);
-                Row row;
-                for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-                    row = sheet.getRow(i);
-                    int id = (int) row.getCell(0).getNumericCellValue();
-                    String user = row.getCell(1).getStringCellValue();
-                    String pass = row.getCell(2).getStringCellValue();
-                    String rol = row.getCell(3).getStringCellValue();
+                String[] rowData;
+
+                int i = 0;
+                String[] data;
+
+                while ((rowData = reader.readNext()) != null) {
+                    data = rowData[0].split(";");
+                    int id = Integer.parseInt(data[0]);
+                    String user = data[1];
+                    String pass = data[2];
+                    String rol = data[3];
                     String sql = "INSERT INTO USERS VALUES(" + id + ",'" + user + "','" + pass
                             + "','" + rol + "')";
+
                     pstm = (PreparedStatement) con.prepareStatement(sql);
                     pstm.execute();
                     System.out.println("Importando fila: " + i);
+                    i++;
                 }
                 con.commit();
                 pstm.close();
+
             }
-            input.close();
-            System.out.println("Importar de Excel a SQL terminado");
-        } catch (ClassNotFoundException | SQLException | IOException e) {
+            System.out.println("Importar de CSV a SQL terminado");
+        } catch (ClassNotFoundException | SQLException | IOException | NumberFormatException e) {
             System.out.println(e);
         }
 
