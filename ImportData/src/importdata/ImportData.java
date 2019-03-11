c49ace69128aa54fb96d8641ee05996263459d20
victorcanno <lpdaska@gmail.com>
2019-03-10 19:32:53 -0500

    Excel Mejorado
    Se añade el importar datos de usuarios

--- ImportData/src/importdata/ImportData.java
+++ ImportData/src/importdata/ImportData.java
@@ -21,44 +21,11 @@ public class ImportData {
      * @param args
      */
     public static void main(String[] args) {
-        try{
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
-            FileInputStream input;
-            try (Connection con = (Connection) DriverManager.getConnection("jdbc:derby://localhost:1527/RecogidasDB","app","app")) {
-                con.setAutoCommit(false);
-                PreparedStatement pstm = null ;
-                input = new FileInputStream("src//resources/PAQUETES.xls");
-                POIFSFileSystem fs = new POIFSFileSystem( input );
-                HSSFWorkbook wb = new HSSFWorkbook(fs);
-                HSSFSheet sheet = wb.getSheetAt(0);
-                Row row;
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                for(int i=1; i<=sheet.getLastRowNum(); i++){
-                    row = sheet.getRow(i);
-                    int id = (int) row.getCell(0).getNumericCellValue();
-                    Date datfechare = row.getCell(1).getDateCellValue();     
-                    String fechare = sdf.format(datfechare);
-                    Date datfechaes = row.getCell(2).getDateCellValue();
-                    String fechaes = sdf.format(datfechaes);
-                    String ciudador = row.getCell(3).getStringCellValue();
-                    String ciudaddes = row.getCell(4).getStringCellValue();
-                    String direccion = row.getCell(5).getStringCellValue();
-                    String destina = row.getCell(6).getStringCellValue();
-                    String estado = row.getCell(7).getStringCellValue();
-                    int idencar = (int) row.getCell(8).getNumericCellValue();
-                    String sql = "INSERT INTO PAQUETES VALUES("+id+",'"+fechare+"','"+fechaes+
-                            "','"+ciudador+"','"+ciudaddes+"','"+direccion+"','"+destina+"','"+estado+"',"+idencar+")";
-                    pstm = (PreparedStatement) con.prepareStatement(sql);
-                    pstm.execute();
-                    System.out.println("Importando fila: "+i);
-                }   con.commit();
-                pstm.close();
-            }
-            input.close();
-            System.out.println("Importar de Excel a SQL terminado");
-        }catch(ClassNotFoundException | SQLException | IOException e){
-            System.out.println(e);
-        }
+        
+        Importar imp = new Importar();
+        
+        imp.ImportarPaquetes("jdbc:derby://localhost:1527/RecogidasDB");
+        imp.ImportarUsuarios("jdbc:derby://localhost:1527/RecogidasDB");
 
     }
 
