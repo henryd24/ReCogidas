5fe0806fe903a5cd1a45d1424e9e9ed6975994d4
victorcanno <lpdaska@gmail.com>
2019-03-10 19:03:34 -0500

    Excel a SQL
    Se termina la importación de Excel a Derby

--- ImportData/src/importdata/ImportData.java
+++ ImportData/src/importdata/ImportData.java
@@ -2,16 +2,17 @@ package importdata;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Row;
-
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
 
 
 public class ImportData {
@@ -21,30 +22,40 @@ public class ImportData {
      */
     public static void main(String[] args) {
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
             FileInputStream input;
             try (Connection con = (Connection) DriverManager.getConnection("jdbc:derby://localhost:1527/RecogidasDB","app","app")) {
                 con.setAutoCommit(false);
                 PreparedStatement pstm = null ;
-                input = new FileInputStream("D://file.xls");
+                input = new FileInputStream("src//resources/PAQUETES.xls");
                 POIFSFileSystem fs = new POIFSFileSystem( input );
                 HSSFWorkbook wb = new HSSFWorkbook(fs);
                 HSSFSheet sheet = wb.getSheetAt(0);
                 Row row;
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
                 for(int i=1; i<=sheet.getLastRowNum(); i++){
                     row = sheet.getRow(i);
                     int id = (int) row.getCell(0).getNumericCellValue();
-                    String name = row.getCell(1).getStringCellValue();
-                    String address = row.getCell(2).getStringCellValue();
-                    String sql = "INSERT INTO PAQUETES VALUES('"+id+"','"+name+"','"+address+"')";
+                    Date datfechare = row.getCell(1).getDateCellValue();     
+                    String fechare = sdf.format(datfechare);
+                    Date datfechaes = row.getCell(2).getDateCellValue();
+                    String fechaes = sdf.format(datfechaes);
+                    String ciudador = row.getCell(3).getStringCellValue();
+                    String ciudaddes = row.getCell(4).getStringCellValue();
+                    String direccion = row.getCell(5).getStringCellValue();
+                    String destina = row.getCell(6).getStringCellValue();
+                    String estado = row.getCell(7).getStringCellValue();
+                    int idencar = (int) row.getCell(8).getNumericCellValue();
+                    String sql = "INSERT INTO PAQUETES VALUES("+id+",'"+fechare+"','"+fechaes+
+                            "','"+ciudador+"','"+ciudaddes+"','"+direccion+"','"+destina+"','"+estado+"',"+idencar+")";
                     pstm = (PreparedStatement) con.prepareStatement(sql);
                     pstm.execute();
-                    System.out.println("Import rows "+i);
+                    System.out.println("Importando fila: "+i);
                 }   con.commit();
                 pstm.close();
             }
             input.close();
-            System.out.println("Success import excel to mysql table");
+            System.out.println("Importar de Excel a SQL terminado");
         }catch(ClassNotFoundException | SQLException | IOException e){
             System.out.println(e);
         }
