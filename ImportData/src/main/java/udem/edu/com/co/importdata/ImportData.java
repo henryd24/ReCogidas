40915e314db0f1b561cd61e179b7e5a6202d1d81
victorcanno <lpdaska@gmail.com>
2019-03-12 18:45:29 -0500

    ImportData a Maven
    Se cambia el proyecto a Maven para el manejo de dependencias

--- ImportData/src/main/java/udem/edu/com/co/importdata/ImportData.java
+++ ImportData/src/main/java/udem/edu/com/co/importdata/ImportData.java
@@ -1,18 +1,21 @@
-package importdata;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udem.edu.com.co.importdata;
 
-
+/**
+ *
+ * @author ASUS
+ */
 public class ImportData {
-
-    /**
-     * @param args
-     */
+    
     public static void main(String[] args) {
         
         Importar imp = new Importar();
-        
         imp.ImportarPaquetes("jdbc:derby://localhost:1527/RecogidasDB");
         imp.ImportarUsuarios("jdbc:derby://localhost:1527/RecogidasDB");
-
     }
-
+    
 }
