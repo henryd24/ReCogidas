aa1e5518194893fa0a897f470f085970ca23ab50
jagg98 <alejandrogarciagomeztst@gmail.com>
2019-03-12 22:25:07 -0500

    Cambion sin interface de Usuario

--- ReCogidas/build/generated-sources/ap-source-output/Entitys/Paquetes_.java
+++ ReCogidas/build/generated-sources/ap-source-output/Entitys/Paquetes_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-10T15:20:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-10T15:37:04")
 @StaticMetamodel(Paquetes.class)
 public class Paquetes_ { 
 
--- ReCogidas/build/generated-sources/ap-source-output/Entitys/Users_.java
+++ ReCogidas/build/generated-sources/ap-source-output/Entitys/Users_.java
@@ -4,12 +4,13 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-10T15:20:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-10T15:37:04")
 @StaticMetamodel(Users.class)
 public class Users_ { 
 
     public static volatile SingularAttribute<Users, String> password;
     public static volatile SingularAttribute<Users, Integer> id;
+    public static volatile SingularAttribute<Users, String> rol;
     public static volatile SingularAttribute<Users, String> username;
 
 }
