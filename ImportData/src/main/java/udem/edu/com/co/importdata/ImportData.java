75d3a2be69b9cfd0b92a3a2274a8666124cd27b2
victorcanno <lpdaska@gmail.com>
2019-03-14 21:37:51 -0500

    CSV
    Se mejora la codificación del proyecto CSV

--- ImportData/src/main/java/udem/edu/com/co/importdata/ImportData.java
+++ ImportData/src/main/java/udem/edu/com/co/importdata/ImportData.java
@@ -8,14 +8,16 @@ package udem.edu.com.co.importdata;
 /**
  *
  * @author ASUS
+ * Clase principal del proyecto para importar al servidor Derby
  */
 public class ImportData {
     
     public static void main(String[] args) {
         
-        Importar imp = new Importar();
-        imp.ImportarPaquetes("jdbc:derby://localhost:1527/RecogidasDB");
-        imp.ImportarUsuarios("jdbc:derby://localhost:1527/RecogidasDB");
+        Importar imp = new Importar(); //Se crea una instancia de la clase importar para acceder a sus metodos
+        imp.ImportarPaquetes("jdbc:derby://localhost:1527/RecogidasDB");  //Se debe modificar la dirección dependiendo
+        imp.ImportarUsuarios("jdbc:derby://localhost:1527/RecogidasDB");   //donde de donde corra el servidor
+        
     }
     
 }
